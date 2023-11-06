20b08362be4ff34bda575fc3a3a8030d61321216
alexalaver <alex.alaverdyan.04@gmail.com>
2023-11-06 20:43:38 +0400

    123

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ bot = Bot(token=cfg.TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-telethon_client = TelegramClient(StringSession(cfg.STRING_SESSION), cfg.API_ID, cfg.API_HASH)
+telethon_client = TelegramClient(StringSession(), cfg.API_ID,
+                                 cfg.API_HASH)  # Обратите внимание, что StringSession должен быть пустым
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -18,7 +19,7 @@ logging.basicConfig(level=logging.INFO)
 # Username чата для отслеживания
 CHAT_USERNAME = "avitologpro1"  # Используйте username чата без "@"
 
-# Множество для отслеживания уже отправленных сообщений
+# Словарь для отслеживания уже отправленных сообщений
 sent_messages_users = set()
 
 # Флаг для контроля состояния отслеживания
@@ -55,13 +56,17 @@ async def handle_new_message(event):
     user_id = sender.id
 
     if user_id not in sent_messages_users:
+        sent_messages_users.add(user_id)  # Добавляем пользователя в множество
+
+    for user_id in list(sent_messages_users):  # Перебираем множество пользователей
         try:
-            # Отправляем сообщение от аккаунта пользователя через Telethon, а не от бота
+            # Отправляем сообщение от аккаунта пользователя через Telethon
             await telethon_client.send_message(user_id, "Привет.")
-            sent_messages_users.add(user_id)  # Добавляем пользователя в множество
-            await asyncio.sleep(5)  # Задержка перед отправкой следующего сообщения
+            await asyncio.sleep(0.1)  # Небольшая задержка для предотвращения ограничения скорости
         except Exception as e:
             logging.error(f"Ошибка при отправке сообщения пользователю {user_id}: {e}")
+        finally:
+            sent_messages_users.remove(user_id)  # Удаляем пользователя из множества после попытки отправки
 
 
 async def main():
